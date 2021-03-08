cb2d24836ff2e85e22fdaa8190d01c1c27897d30
Mohammed Fareed <52881835+mohdfareed@users.noreply.github.com>
2021-03-08 18:16:44 +0200

    Fix symbolic linking script

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env zsh
 
-symlink_dotfile() {
-  ln -sfv $1 "$HOME/.$1"
+symlink() {
+  ln -siv "$HOME/.dotfiles/dots/$1" "$HOME/.$1"
 }
 
 
 echo "Creating symlinks ..."
 
-pushd $HOME/.dotfiles/dots
-symlink_dotfile zshrc
-symlink_dotfile aliases
-symlink_dotfile gitconfig
-symlink_dotfile gitignore_global
+# symbolically link dot files
+symlink zshrc
+symlink aliases
+symlink gitconfig
+symlink gitignore_global
 
-# oh-my-zsh theme
+# set oh-my-zsh theme
 git -C $HOME/.dotfiles/other/common pull
-ln -s $HOME/.dotfiles/other/common/common.zsh-theme $HOME/.oh-my-zsh/themes
+ln -siv $HOME/.dotfiles/other/common/common.zsh-theme $HOME/.oh-my-zsh/themes
 
 # colorls configuration
 mkdir -p $HOME/.config/colorls
-ln -s $HOME/.dotfiles/other/dark_colors.yaml ~/.config/colorls/dark_colors.yaml
+ln -siv $HOME/.dotfiles/other/dark_colors.yaml \
+  ~/.config/colorls/dark_colors.yaml
