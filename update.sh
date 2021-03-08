94612fb53713f7e3f2898c51d6d86c92329822ad
Mohammed Fareed <52881835+mohdfareed@users.noreply.github.com>
2021-03-08 22:06:13 +0200

    Add updating script

---
+++ update.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env zsh
+
+echo "Updating..."
+
+# update submodules
+git -C $HOME/.dotfiles submodule update --init
+
+# update brew and its forumlae and casks
+brew update
+brew upgrade
+brew cleanup
+
+# update App Store apps
+mas upgrade
+
+# install latest ruby version
+source $HOME/.dotfiles/scripts/ruby.sh
+
+# update oh-my-zsh
+omz update
+
+echo "Updating complete!"
