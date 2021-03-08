23750a80cd8906b3f81d446c68c9f2392c83cf51
Mohammed Fareed <52881835+mohdfareed@users.noreply.github.com>
2021-03-08 17:04:52 +0200

    Update homebrew installation

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -3,8 +3,8 @@ echo "Installing homebrew ..."
 
 which -s brew
 if [[ $? != 0 ]] ; then
-  yes | /usr/bin/ruby -e "$(curl -fsSL \
-    https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL \
+    https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
   echo "Homebrew already installed ..."
 fi
