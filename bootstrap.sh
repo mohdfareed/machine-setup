b44bacc019fb10817a2ac2859093b69c67e55657
Mohammed Fareed <52881835+mohdfareed@users.noreply.github.com>
2021-03-12 18:59:05 +0200

    Setup git in a seperate script
    Let bootstrapping script clone the repo and source other setup scripts.
    
    Setup git in a seperate script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,42 @@
 #!/usr/bin/env zsh
 
+clear='\033[0m'
+bold='\033[1m'
+gbold='\033[1;32m'
+
 dotfiles="$HOME/.dotfiles"
+scripts="$dotfiles/scripts"
 username="mohdfareed" # github username
 pat='3b82f82e9087eb0db848fada1b1f239ee43a46db' # github personal access token
 
 
-# clone dotfiles repo and its submodules
+# clone repo and its submodules
 git clone https://$username:$pat@github.com/mohdfareed/dotfiles.gi
 git -C $dotfiles submodule update --init
 
-# symlink git configuration files
-ln -siv "$dotfiles/dots/gitconfig" "$HOME/.gitconfig"
-ln -siv "$dotfiles/dots/gitignore_global" "$HOME/.gitignore_global"
+# remove last login time prompt
+touch $HOME/.hushlogin
+# create config hidden folder
+mkdir -p $HOME/.config
 
-source $dotfiles/scripts/setup.sh
+# setup zsh
+source $scripts/zsh.sh
+
+# setup homebrew and its forumlae and casks
+source $scripts/homebrew.sh
+source $scripts/homebrew_formulae.sh
+source $scripts/homebrew_casks.sh
+# install App Store applications
+source $scripts/appstore.sh
+
+# setup ruby and its gems
+source $scripts/ruby.sh
+
+# setup git
+source $scripts/git.sh
+
+# setup terminal
+source $scripts/terminal_setup.sh
+
+echo -e "\a"
+echo "${gbold}Installation complete!${clear}"
---
+++ scripts/git.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env zsh
+
+## REQUIREMENTS:
+# brew
+
+clear='\033[0m'
+bold='\033[1m'
+rbold='\033[1;31m'
+
+tput clear
+echo "${bold}Setting up git...${clear}"
+
+# Check for homebrew and setup if needed
+which -s brew
+if [[ $? != 0 ]] ; then
+    echo "${bold}Homebrew is not installed.${clear}"
+    echo "${bold}Would you like to install it?${clear}"
+    echo -e "\a"
+    read -sk "?Press RETURN to continue or any other key to abort" answer
+
+    if [[ $answer != $'\n' ]] ; then
+    exit;
+    fi
+
+    source $dotfiles/scripts/homebrew.sh
+fi
+
+brew install git
+
+# symlink git configuration files
+ln -siv "$dotfiles/dots/gitconfig" "$HOME/.gitconfig"
+ln -siv "$dotfiles/dots/gitignore_global" "$HOME/.gitignore_global"
