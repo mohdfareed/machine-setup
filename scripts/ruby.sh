c8829274a2cdae77de575a411d59861abc6cae9f
Mohammed Fareed <52881835+mohdfareed@users.noreply.github.com>
2021-03-27 00:25:05 +0300

    Update ruby script
    Install rbenv by default.
    Don't prompt to install a ruby version using rbenv.
    Install gems for vscode debugging and ide features with brew's ruby.

--- scripts/ruby.sh
+++ scripts/ruby.sh
@@ -5,6 +5,44 @@ bold='\033[1m'
 rbold='\033[1;31m'
 gbold='\033[1;32m'
 
+tput clear
+echo "${bold}Setting up ruby...${clear}"
+
+# check if brew is installed
+which brew > /dev/null
+if [[ $? != 0 ]] ; then
+    echo "${rbold}Error:${clear} Homebrew is not installed..."
+    return 1
+fi
+# check if DOTFILES directory exists
+if [[ ! -d $DOTFILES ]] ; then
+    echo "${rbold}Error:${clear} DOTFILES directory does not exist..."
+    return 1
+fi
+
+# source zshenv and zshrc
+source $DOTFILES/dots/zshenv
+source $DOTFILES/dots/zshrc > /dev/null
+
+# install ruby and rbenv
+brew install ruby
+brew install rbenv
+brew cleanup
+
+# add gems directory to path
+gemdir="$(gem environment gemdir)/"
+currdir='/usr/local/lib/ruby/gems/.*/'
+sed -i '' "s#$currdir#$gemdir#" "$DOTFILES/dots/zshrc"
+
+echo "${bold}Installing gems...${clear}"
+
+# code completion, documentation, and static analysis
+gem install solargraph
+# an interface which glues ruby-debug to IDEs
+gem install ruby-debug-ide
+# fast implementation of the standard Ruby debugger
+gem install debase -v '>= 0.2.5.beta'
+
 # prompt the user for confirmation, skip installation if needed
 prompt() {
     echo -e "\a"
@@ -20,58 +58,9 @@ prompt() {
     esac
 }
 
-
-tput clear
-echo "${bold}Setting up ruby...${clear}"
-
-# check if brew is installed
-which brew > /dev/null
-if [[ $? != 0 ]] ; then
-    echo "${rbold}Error:${clear} Homebrew is not installed..."
-    return 1
-fi
-
-brew install ruby
-brew install rbenv
-brew cleanup
-
-if [[ ! -d $DOTFILES ]] ; then
-    echo "${rbold}Error:${clear} DOTFILES directory does not exist..."
-    return 1
-fi
-
-source $DOTFILES/dots/zshenv
-source $DOTFILES/dots/zshrc > /dev/null
-
-# prompt the user to selected ruby version to install
-echo -e "\a"
-echo "Which ruby version would you like to install?"
-echo
-echo "${bold}Installed versions:${clear}"
-rbenv versions
-echo "${bold}Available versions:${clear}"
-rbenv install -l 2> /dev/null
-read version
-
-# install selected Ruby version and set it as default
-rbenv install $version
-if [[ $? != 0 ]] ; then
-    return 1
-fi
-rbenv global $version
-rbenv rehash
-eval "$(rbenv init -)" # update the current session's ruby
-
-echo "${bold}Installing gems...${clear}"
-
 prompt pry      # runtime developer console and IRB alternative
 prompt byebug   # Ruby debugger
 prompt rails    # full-stack web framework
 prompt colorize # methods to set text color, background color, text effects
-
-# vscode functionality
-gem install solargraph # code completion, documentation, and static analysis
-gem install ruby-debug-ide # an interface which glues ruby-debug to IDEs
-gem install debase -v 0.2.5.beta2 # fast implementation of the standard Ruby debugger
 
 gem update
