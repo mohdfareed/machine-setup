886fce12a42314304a36cd8de45a73531974e019
Mohammed Fareed <52881835+mohdfareed@users.noreply.github.com>
2021-08-24 05:44:04 +0300

    Simplify macos script and remove automator files
    Switch to shortcuts app.

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -6,18 +6,6 @@ bold='\033[1m'
 tput clear
 echo "${bold}Setting up macOS preferences...${clear}"
 
-# check if dotfiles exists
-if [[ ! -d $DOTFILES ]] ; then
-    echo "${rbold}Error:${clear} DOTFILES directory does not exist..."
-    return 1
-fi
-
-source $DOTFILES/dots/zshenv
-# create link to projects on desktop
-if [[ -d $DEV/projects ]] ; then
-	ln -siv $DEV/projects ~/Desktop/Projects
-fi
-
 # close open System Preferences panes, to prevent them from overriding settings
 osascript -e 'tell application "System Preferences" to quit'
 # ask for the administrator password upfront
@@ -53,14 +41,10 @@ defaults write com.apple.AppleMultitouchTrackpad Dragging -bool true
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Dragging -bool true
 # mouse acceleration
 defaults write .GlobalPreferences com.apple.mouse.scaling -int -1
-
 # key repeat
 defaults write .GlobalPreferences KeyRepeat -int 2
 # delay until key repeat
 defaults write .GlobalPreferences InitialKeyRepeat -int 25
-
-# Extensions
-# ==========
 
 # Terminal
 # ========
@@ -69,7 +53,6 @@ defaults write .GlobalPreferences InitialKeyRepeat -int 25
 p_file=$(find $DOTFILES/other -maxdepth 1 -name "*.terminal" | head -n 1)
 p_name=$(basename $p_file .terminal)
 open -g $p_file # add profile to terminal app
-
 # default Terminal profile
 defaults write com.apple.Terminal "Default Window Settings" "$p_name"
 defaults write com.apple.Terminal "Startup Window Settings" "$p_name"
@@ -84,18 +67,18 @@ defaults write com.apple.finder NewWindowTarget 'PfHm'
 defaults write com.apple.finder NewWindowTargetPath "file://${HOME}/"
 # when performing a search, use the current folder by default
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
-# show the ~/Library folder
-chflags nohidden ~/Library && xattr -d com.apple.FinderInfo ~/Library
-# avoid creating .DS_Store files on network volumes
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 # keep folders on top when sorting by name
 defaults write com.apple.finder _FXSortFoldersFirst -bool true
 defaults write com.apple.finder _FXSortFoldersFirstOnDesktop -bool true
+# show the ~/Library folder
+chflags nohidden ~/Library && xattr -d com.apple.FinderInfo ~/Library
 # enable snap-to-grid for icons on the desktop and in other icon views
 /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
 /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
 # use list view in all Finder windows by default
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+# avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
 # TextEdit
 # ========
