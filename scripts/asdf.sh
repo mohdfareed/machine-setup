e41b1a30c939e0802474b46942588e3f7f1da13e
Mohammed Fareed <52881835+mohdfareed@users.noreply.github.com>
2021-08-30 03:03:57 +0300

    Update asdf script
    Create directories.
    Fix formatting.
    Be more DRY.

--- scripts/asdf.sh
+++ scripts/asdf.sh
@@ -2,20 +2,27 @@
 
 clear='\033[0m'
 bold='\033[1m'
+source $DOTFILES/dots/zshenv
 
-# setup asdf config file
-mkdir -p $XDG_CONFIG_HOME/asdf
-ln -siv "$DOTFILES/dots/asdfrc" "$XDG_CONFIG_HOME/asdf/asdfrc"
+# link asdfrc
+mkdir -p $(dirname $ASDF_CONFIG_FILE)
+ln -siv "$DOTFILES/dots/asdfrc" "$ASDF_CONFIG_FILE"
+
+# python setup
+# ============
 
 tput clear
 echo "${bold}Setting up python...${clear}"
 # link pythonrc
-mkdir -p $XDG_CONFIG_HOME/python
+mkdir -p $(dirname $PYTHONSTARTUP)
 ln -siv "$DOTFILES/dots/pythonrc" "$PYTHONSTARTUP"
 # installed latest python version and set it as default
 asdf plugins add python
 asdf install python latest > /dev/null
 asdf global python latest
+
+# ruby setup
+# ==========
 
 echo
 echo "${bold}Setting up ruby...${clear}"
@@ -27,28 +34,39 @@ asdf plugin add ruby
 asdf install ruby latest > /dev/null
 asdf global ruby latest
 
+# node setup
+# ==========
+
 echo
 echo "${bold}Setting up nvm...${clear}"
-mkdir -p "$XDG_DATA_HOME/node" # create repl history directory
-brew install gpg               # dependency
+# create needed directories
+mkdir -p $(dirname $NPM_CONFIG_USERCONFIG)
+mkdir -p $(dirname $NODE_REPL_HISTORY)
+mkdir -p $NPM_CONFIG_CACHE
 # installed latest node version and set it as default
 asdf plugin add nodejs
 asdf install nodejs latest > /dev/null
 asdf global nodejs latest
 
+# sqlite setup
+# ============
+
 echo
 echo "${bold}Setting up sqlite...${clear}"
-mkdir -p "$XDG_DATA_HOME/sqlite" # create history directory
+mkdir -p $(dirname $SQLITE_HISTORY) # create history directory
 # installed latest sqlite version and set it as default
 asdf plugin add sqlite
 asdf install sqlite latest > /dev/null
 asdf global sqlite latest
 
+# postgresql setup
+# ============
+
 echo
 echo "${bold}Setting up postgresql...${clear}"
 # create needed directories
-mkdir "$XDG_CONFIG_HOME/psql"
-mkdir "$XDG_DATA_HOME/psql"
+mkdir $(dirname $PSQLRC)
+mkdir $(dirname $PSQL_HISTORY)
 #  compile with openssl libraries
 POSTGRES_EXTRA_CONFIGURE_OPTIONS="--with-uuid=e2fs --with-openssl \
 --with-libraries=/usr/local/lib:$(brew --prefix openssl)/lib \
