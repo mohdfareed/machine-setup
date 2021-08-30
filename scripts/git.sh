ade2e724983d04d347b923500f6736c1ee44f853
Mohammed Fareed <52881835+mohdfareed@users.noreply.github.com>
2021-08-30 17:03:27 +0300

    Add gpg key setup

--- scripts/git.sh
+++ scripts/git.sh
@@ -11,3 +11,11 @@ source $DOTFILES/dots/zshenv
 mkdir -p $XDG_CONFIG_HOME/git
 ln -siv "$DOTFILES/dots/gitconfig" "$XDG_CONFIG_HOME/git/config"
 ln -siv "$DOTFILES/dots/gitignore" "$XDG_CONFIG_HOME/git/ignore"
+
+# setup gpg key
+mkdir -pm 0700 $GNUPGHOME
+echo "pinentry-program /usr/local/bin/pinentry-mac" >> $GNUPGHOME/gpg-agent.conf
+
+echo "Passphrase: ${bold}iJDKCS6qn59o${clear}"
+gpg --import $DOTFILES/resources/gpg_private.key > /dev/null
+echo "test" | gpg --clearsign > /dev/null
