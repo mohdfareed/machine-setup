eb38623d807611f505b29e5aeb6c45570c7e0ecb
Mohammed Fareed <52881835+mohdfareed@users.noreply.github.com>
2021-08-31 22:37:13 +0300

    Bootstrap only essential scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,13 +2,11 @@
 
 clear='\033[0m'
 bold='\033[1m'
-gbold='\033[1;32m'
 
 DOTFILES="$HOME/Developer/dotfiles"
-scripts="$DOTFILES/scripts"
 
-username="mohdfareed"                           # github username
-pat=$(cat $DOTFILES/resources/github_token.txt) # github personal access token
+username="mohdfareed"                          # github username
+pat="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" # github personal access token
 
 tput clear
 echo "Cloning dotfiles repo..."
@@ -17,12 +15,9 @@ git clone https://$username:$pat@github.com/mohdfareed/dotfiles.git $DOTFILES
 git -C $DOTFILES submodule update --init
 
 source $scripts/homebrew.sh # setup homebrew
-source $scripts/zsh.sh      # setup zsh
+source $zsh/zsh.sh          # setup zsh
 source $git/git.sh          # setup git
-source $asdf/asdf.sh        # setup asdf
-source $scripts/appstore.sh # install App Store applications
-source $scripts/macos.sh    # setup macOS preferences
 
 echo -e "\a"
 echo "Restart for some of the changes to take effect."
-echo "${gbold}Setup complete!${clear}"
+echo "${bold}Setup complete!${clear}"
