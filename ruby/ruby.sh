958db07b9cd5ccff0f5176b5bf3c5534316f5cca
Mohammed Fareed <52881835+mohdfareed@users.noreply.github.com>
2021-09-12 13:56:14 +0300

    Set default gems for every ruby installation

--- ruby/ruby.sh
+++ ruby/ruby.sh
@@ -13,16 +13,6 @@ source "$(dirname "$ruby_dir")/zsh/zshenv"
 asdf plugin add ruby
 asdf install ruby latest > /dev/null
 asdf global ruby latest
-
-# install gems
-
-gem install bundler # manages an app's dependencies through its entire life
-gem install pry     # runtime developer console and IRB alternative
-gem install byebug  # ruby debugger
-
-gem install solargraph     # code completion, documentation, and static analysis
-gem install ruby-debug-ide # an interface which glues ruby-debug to IDEs
-gem install debase -v '>= 0.2.5.beta' # implementation of standard Ruby debugger
 asdf reshim
 
 # add environment variables
