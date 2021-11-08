91f0d433d7107096438cb4373c8b48372113d902
Mohammed Fareed <52881835+mohdfareed@users.noreply.github.com>
2021-11-08 03:51:02 +0200

    Fix appstore script
    Install lighter applications first.
    
    Remove account check as it does not work on Monterey.

--- macos/appstore.sh
+++ macos/appstore.sh
@@ -8,12 +8,6 @@ tput clear
 echo "${bold}Installing App Store applications${clear}"
 
 brew install mas
-# check if the user is signed-in to the App Store
-mas account >/dev/null
-if [[ $? != 0 ]]; then
-    echo "${red}Error:${clear} You need to be signed-in to the App Store"
-    return 1
-fi
 
 # install apps
 mas install 1487937127 # Craft Docs
@@ -22,8 +16,8 @@ mas install 1438243180 # Dark Reader
 mas install 1320666476 # Wipr
 mas install 937984704  # Amphetamine
 mas install 517914548  # Dashlane
-mas install 497799835  # Xcode
+mas install 1554235898 # Peek
 mas install 409201541  # Pages
 mas install 409203825  # Numbers
 mas install 409183694  # Keynote
-mas install 1554235898 # Peek
+mas install 497799835  # Xcode
