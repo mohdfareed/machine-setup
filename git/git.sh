42aa0b91840d5f15ce0e5aa5cd671030ce225fe9
Mohammed Fareed <52881835+mohdfareed@users.noreply.github.com>
2021-11-08 03:52:48 +0200

    Fix git script formatting

--- git/git.sh
+++ git/git.sh
@@ -20,9 +20,10 @@ ln -sfv "$git_dir/gitignore" "$XDG_CONFIG_HOME/git/ignore"
 
 # setup gpg key
 mkdir -pm 0700 "$GNUPGHOME"
-echo "pinentry-program /usr/local/bin/pinentry-mac" >> "$GNUPGHOME/gpg-agent.conf"
+echo "pinentry-program /usr/local/bin/pinentry-mac" >>"$GNUPGHOME/gpg-agent.conf"
 echo "Passphrase: ${bold}WYy85#1X3dP#OlOS${clear}"
+
 # import key and fix owner trust level
-gpg --import "$git_dir/gpg-public.key" > /dev/null
-gpg --allow-secret-key-import --import "$git_dir/gpg-private.key" > /dev/null
-gpg --import-ownertrust "$git_dir/gpg-ownertrust" > /dev/null
+gpg --import "$git_dir/gpg-public.key" >/dev/null
+gpg --allow-secret-key-import --import "$git_dir/gpg-private.key" >/dev/null
+gpg --import-ownertrust "$git_dir/gpg-ownertrust" >/dev/null
