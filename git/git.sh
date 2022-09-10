38571f648b1c78fc648dd9949b79d030cb2f93ff
Mohammed Fareed <mohdf.fareed@icloud.com>
2022-09-09 22:41:48 -0400

    Update git gpg signing keys
    - Switch to using main email on git and github
    - New gpg heys were added for personal and RIT emails
    - Personal email is set as the default

--- git/git.sh
+++ git/git.sh
@@ -21,9 +21,14 @@ ln -sfv "$git_dir/gitignore" "$XDG_CONFIG_HOME/git/ignore"
 # setup gpg key
 mkdir -pm 0700 "$GNUPGHOME"
 echo "pinentry-program /opt/homebrew/bin/pinentry-mac" >>"$GNUPGHOME/gpg-agent.conf"
-echo "Passphrase: ${bold}WYy85#1X3dP#OlOS${clear}"
+echo "Passphrase: ${bold}AC#s!7cdSXXP6?hM${clear}"
 
-# import key and fix owner trust level
-gpg --import "$git_dir/gpg-public.key" >/dev/null
-gpg --allow-secret-key-import --import "$git_dir/gpg-private.key" >/dev/null
+# import keys and fix owner trust level
+
+gpg --import "$git_dir/public.key" >/dev/null
+gpg --import "$git_dir/private.key" >/dev/null
+
+gpg --import "$git_dir/rit-public.key" >/dev/null
+gpg --import "$git_dir/rit-private.key" >/dev/null
+
 gpg --import-ownertrust "$git_dir/gpg-ownertrust" >/dev/null
