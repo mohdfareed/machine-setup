99180a83b50680e56aa13ec0ea04b5cc4bbfb319
Mohammed Fareed <mohdf.fareed@icloud.com>
2023-02-26 21:32:08 -0500

    Format git script and gitconfig

--- git/git.sh
+++ git/git.sh
@@ -6,29 +6,33 @@ bold='\033[1m'
 tput clear
 echo "${bold}Setting up git...${clear}"
 
+# path to git scripts directory
 git_dir=$(dirname "${0:a}")
+# load zsh environment variables
 source "$(dirname "$git_dir")/zsh/zshenv"
 
-brew install git
-brew install gpg
-brew install pinentry-mac
+# install required packages
+brew install git          # main git installation of the system
+brew install gpg          # gpg package for signing commits
+brew install pinentry-mac # pinentry for gpg
 
 # symlink git configuration files
-mkdir -p "$XDG_CONFIG_HOME/git"
-ln -sfv "$git_dir/gitconfig" "$XDG_CONFIG_HOME/git/config"
-ln -sfv "$git_dir/gitignore" "$XDG_CONFIG_HOME/git/ignore"
+mkdir -p "$XDG_CONFIG_HOME/git" # create git config directory
+ln -sfv "$git_dir/gitconfig" "$XDG_CONFIG_HOME/git/config" # git config file
+ln -sfv "$git_dir/gitignore" "$XDG_CONFIG_HOME/git/ignore" # global ignore file
 
 # setup gpg key
-mkdir -pm 0700 "$GNUPGHOME"
-echo "pinentry-program /opt/homebrew/bin/pinentry-mac" >>"$GNUPGHOME/gpg-agent.conf"
+mkdir -pm 0700 "$GNUPGHOME" # create gpg home directory
+echo "pinentry-program /opt/homebrew/bin/pinentry-mac" >> \
+     "$GNUPGHOME/gpg-agent.conf"
 echo "Passphrase: ${bold}AC#s!7cdSXXP6?hM${clear}"
 
-# import keys and fix owner trust level
-
+# import personal key pair
 gpg --import "$git_dir/public.key" >/dev/null
 gpg --import "$git_dir/private.key" >/dev/null
-
+# import RIT key pair
 gpg --import "$git_dir/rit-public.key" >/dev/null
 gpg --import "$git_dir/rit-private.key" >/dev/null
 
+# fix owner trust level
 gpg --import-ownertrust "$git_dir/gpg-ownertrust" >/dev/null
