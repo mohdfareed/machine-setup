816d365e76f9f570a1a30e3772d093615e8fbaf9
Mohammed Fareed <mohdf.fareed@icloud.com>
2023-03-17 23:52:41 -0400

    Formatted `setup.py` and `colors.py`

--- colors.py
+++ colors.py
@@ -1,77 +1,91 @@
-class color:
-    """Color class for printing in color
-    """
+reset = '\u001b[0m'
+bold = '\u001b[01m'
+underline = '\u001b[04m'
+reverse = '\u001b[07m'
+strikethrough = '\u001b[09m'
 
-    reset = '\u001b[0m'
-    bold = '\u001b[01m'
-    underline = '\u001b[04m'
-    reverse = '\u001b[07m'
-    strikethrough = '\u001b[09m'
 
-    class fg:
-        black = '\u001b[30m'
-        red = '\u001b[31m'
-        green = '\u001b[32m'
-        yellow = '\u001b[33m'
-        blue = '\u001b[34m'
-        magenta = '\u001b[35m'
-        cyan = '\u001b[36m'
-        white = '\u001b[37m'
-        brightBlack = '\u001b[30;1m'
-        brightRed = '\u001b[31;1m'
-        brightGreen = '\u001b[32;1m'
-        brightYellow = '\u001b[33;1m'
-        brightBlue = '\u001b[34;1m'
-        brightMagenta = '\u001b[35;1m'
-        brightCyan = '\u001b[36;1m'
-        brightWhite = '\u001b[37;1m'
+class Color:
+    black = '\u001b[30m'
+    red = '\u001b[31m'
+    green = '\u001b[32m'
+    yellow = '\u001b[33m'
+    blue = '\u001b[34m'
+    magenta = '\u001b[35m'
+    cyan = '\u001b[36m'
+    white = '\u001b[37m'
+    brightBlack = '\u001b[30;1m'
+    brightRed = '\u001b[31;1m'
+    brightGreen = '\u001b[32;1m'
+    brightYellow = '\u001b[33;1m'
+    brightBlue = '\u001b[34;1m'
+    brightMagenta = '\u001b[35;1m'
+    brightCyan = '\u001b[36;1m'
+    brightWhite = '\u001b[37;1m'
 
-    class bg:
-        black = '\u001b[40m'
-        red = '\u001b[41m'
-        green = '\u001b[42m'
-        yellow = '\u001b[44m'
-        blue = '\u001b[44m'
-        magenta = '\u001b[45m'
-        cyan = '\u001b[46m'
-        white = '\u001b[47m'
-        brightBlack = '\u001b[40;1m'
-        brightRed = '\u001b[41;1m'
-        brightGreen = '\u001b[42;1m'
-        brightYellow = '\u001b[44;1m'
-        brightBlue = '\u001b[44;1m'
-        brightMagenta = '\u001b[45;1m'
-        brightCyan = '\u001b[46;1m'
-        brightWhite = '\u001b[47;1m'
 
-    def Bold(text): return color.bold + text + color.reset
+class BG:
+    black = '\u001b[40m'
+    red = '\u001b[41m'
+    green = '\u001b[42m'
+    yellow = '\u001b[44m'
+    blue = '\u001b[44m'
+    magenta = '\u001b[45m'
+    cyan = '\u001b[46m'
+    white = '\u001b[47m'
+    brightBlack = '\u001b[40;1m'
+    brightRed = '\u001b[41;1m'
+    brightGreen = '\u001b[42;1m'
+    brightYellow = '\u001b[44;1m'
+    brightBlue = '\u001b[44;1m'
+    brightMagenta = '\u001b[45;1m'
+    brightCyan = '\u001b[46;1m'
+    brightWhite = '\u001b[47;1m'
 
-    def Underline(text): return color.underline + text + color.reset
 
-    def Reverse(text): return color.reverse + text + color.reset
+def Bold(text): return bold + text + reset
 
-    def Strikethrough(text): return color.strikethrough + text + color.reset
 
-    def Red(text): return color.fg.red + text + color.reset
+def Underline(text): return underline + text + reset
 
-    def Green(text): return color.fg.green + text + color.reset
 
-    def Yellow(text): return color.fg.yellow + text + color.reset
+def Reverse(text): return reverse + text + reset
 
-    def Blue(text): return color.fg.blue + text + color.reset
 
-    def Magenta(text): return color.fg.magenta + text + color.reset
+def Strikethrough(text): return strikethrough + text + reset
 
-    def Cyan(text): return color.fg.cyan + text + color.reset
 
-    def BrightRed(text): return color.fg.brightRed + text + color.reset
+def Red(text): return Color.red + text + reset
 
-    def BrightGreen(text): return color.fg.brightGreen + text + color.reset
 
-    def BrightYellow(text): return color.fg.brightYellow + text + color.reset
+def Green(text): return Color.green + text + reset
 
-    def BrightBlue(text): return color.fg.brightBlue + text + color.reset
 
-    def BrightMagenta(text): return color.fg.brightMagenta + text + color.reset
+def Yellow(text): return Color.yellow + text + reset
 
-    def BrightCyan(text): return color.fg.brightCyan + text + color.reset
+
+def Blue(text): return Color.blue + text + reset
+
+
+def Magenta(text): return Color.magenta + text + reset
+
+
+def Cyan(text): return Color.cyan + text + reset
+
+
+def BrightRed(text): return Color.brightRed + text + reset
+
+
+def BrightGreen(text): return Color.brightGreen + text + reset
+
+
+def BrightYellow(text): return Color.brightYellow + text + reset
+
+
+def BrightBlue(text): return Color.brightBlue + text + reset
+
+
+def BrightMagenta(text): return Color.brightMagenta + text + reset
+
+
+def BrightCyan(text): return Color.brightCyan + text + reset
--- setup.py
+++ setup.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python3
-print("Hello World!")
+from colors import Bold, BrightRed, BrightGreen
+
+
+def main():
+    info("Setting up Homebrew...")
+
+
+
+def error(message): print(BrightRed(message))
+
+
+def success(message): print(BrightGreen(message))
+
+
+def info(message): print(Bold(message))
+
+
+if __name__ == "__main__":
+    main()
