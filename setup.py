c09ae3d5b6e314e3e21516577fcfdc34fd6a5d32
Mohammed Fareed <mohdf.fareed@icloud.com>
2023-03-19 02:19:07 -0400

    Formatted `setup.py`

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-import scripts.homebrew as homebrew
+import scripts.lib.display as display
 import scripts.git as git
-import scripts.zsh as zsh
-import scripts.python as python
+import scripts.homebrew as homebrew
 import scripts.macos as macos
-from scripts.lib import display
+import scripts.python as python
+import scripts.zsh as zsh
 
 verbose: bool = False
 """Print verbose messages."""
@@ -15,8 +15,16 @@ debug: bool = False
 
 def main():
     # set display mode
-    display.verbose_mode = verbose
     display.debug_mode = debug
+    display.verbose_mode = verbose
+    # print setup display mode
+    if debug:
+        display.debug("Debug mode is enabled.")
+    if verbose:
+        display.verbose("Verbose mode is enabled.")
+    # print setup header
+    display.header("Setting up machine...")
+
     # setup components
     homebrew.setup()
     git.setup()
@@ -24,18 +32,26 @@ def main():
     python.setup()
     macos.setup()
 
+    # inform user that setup is complete and a restart is required
+    display.print("")
+    display.success("Machine setup complete!")
+    display.info("Please restart your machine for some changes to apply.")
+
 
 if __name__ == "__main__":
     import argparse
+
     # parse command line arguments
     parser = argparse.ArgumentParser(description="Machine setup script.")
     parser.add_argument("-v", "--verbose", action="store_true",
                         help="print verbose messages")
     parser.add_argument("-d", "--debug", action="store_true",
-                        help="print debug messages",)
+                        help="print debug messages")
     args = parser.parse_args()
+
     # assign arguments to global variables
     verbose = args.verbose
     debug = args.debug
+
     # run main function
     main()
