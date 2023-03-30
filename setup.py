4f1d12d31f502e099609d0e958f95f35b53cebff
Mohammed Fareed <mohdf.fareed@icloud.com>
2023-03-30 19:33:19 -0400

    Restructure `setup`

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-from setup_modules import homebrew, git, zsh, python, macos
+import os
+from typing import Callable
+
+from setup_modules import git, homebrew, macos, python, zsh
 from utils.display import Display
 from utils.shell import Shell
-from typing import Callable
-import os
 
 silent: bool = False
 """Do not prompt the user during setup."""
@@ -15,93 +16,97 @@ verbose: bool = True
 debug: bool = True
 """Print debug output."""
 
+_display: Display
+"""The display for printing messages."""
+_shell: Shell
+"""The shell for running commands."""
+
 
 def main() -> None:
     """Run the main function of the setup script. This function is called when
     the script is run from the command line. It will prompt the user to run
     a setup function for every setup module. By default, the setup is run in
     verbose and debug mode without logging output to a file."""
+    global _display, _shell
+
     # set the working directory to the directory of this file
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    display, shell = _init()  # initialize display and shell
-    display.header("Setting up machine...")
+    _init()  # initialize display and shell
+    _display.header("Setting up machine...")
 
     # check if git is installed
-    if shell.run('command -v git', display.debug, display.error) != 0:
-        display.error("Git is not installed.")
+    if _shell.run('command -v git', _display.debug, _display.error) != 0:
+        _display.error("Git is not installed.")
         exit(1)
-    display.verbose("Git installation found.")
+    _display.verbose("Git installation found.")
 
     # get resources if not already present
-    if shell.run_quiet('git submodule update --init --recursive --remote',
-                       display.verbose, "Initializing resources") != 0:
-        display.error("Failed to initialize resources.")
+    if _shell.run_quiet('git submodule update --init --recursive --remote',
+                        _display.verbose, "Initializing resources") != 0:
+        _display.error("Failed to initialize resources.")
         exit(1)
-    display.success("Resources initialized.")
+    _display.success("Resources initialized.")
 
     # prompt user to setup components
-    _prompt_setup(homebrew.setup, "Homebrew", display, shell)
-    _prompt_setup(zsh.setup, "Zsh", display, shell)
-    _prompt_setup(git.setup, "Git", display, shell)
-    _prompt_setup(python.setup, "Python", display, shell)
-    _prompt_setup(macos.setup, "macOS", display, shell)
+    _prompt_setup(homebrew.setup, "Homebrew")
+    _prompt_setup(zsh.setup, "Zsh")
+    _prompt_setup(git.setup, "Git")
+    _prompt_setup(python.setup, "Python")
+    _prompt_setup(macos.setup, "macOS")
 
-    # inform user that setup is complete and a restart is required
-    display.success("")
-    display.success("Machine setup complete!")
-    display.info("Please restart your machine for some changes to apply.")
+    _display.success("")
+    _display.success("Machine setup complete!")
+    _display.info("Please restart your machine for some changes to apply.")
 
 
-def _init() -> tuple[Display, Shell]:
-    """Initialize a `Display` and `Shell` for the setup script and prints the
+def _init():
+    """Initialize the `Display` and `Shell` for the setup script and prints the
     display mode. If the script is run in silent mode, it will prompt the user
     for their password to get sudo privileges.
-
-    Returns:
-        tuple[Display, Shell]: A list containing the `Display` and `Shell`.
     """
+    global silent, no_logging, verbose, debug, _display, _shell
 
     # create a shell instance and set display mode
-    display = Display(verbose, debug, no_logging)
-    shell = Shell()
+    _display = Display(verbose, debug, no_logging)
+    _shell = Shell()
     # print setup display mode
-    display.debug("Debug mode is enabled.") if debug else None
-    display.verbose("Verbose mode is enabled.") if verbose else None
-    display.info("Logging is disabled.") if no_logging else None
+    _display.debug("Debug mode is enabled.") if debug else None
+    _display.verbose("Verbose mode is enabled.") if verbose else None
+    _display.info("Logging is disabled.") if no_logging else None
     if not silent:
-        return display, shell
+        return
 
     # prompt user for sudo privileges if running in silent mode
-    display.warning("Silent mode is enabled.")
-    display.header("Sudo privileges are required to run in silent mode.")
+    _display.warning("Silent mode is enabled.")
+    _display.header("Sudo privileges are required to run in silent mode.")
     # create a shell instance with sudo privileges
     try:
-        shell = Shell(sudo=True)
+        _shell = Shell(sudo=True)
     except PermissionError:
-        display.error("Failed to get sudo privileges.")
+        _display.error("Failed to get sudo privileges.")
         exit(1)
-    display.verbose("Sudo privileges granted.")
-
-    return display, shell
+    _display.verbose("Sudo privileges granted.")
 
 
-def _prompt_setup(procedure: Callable, name: str,
-                  display: Display, shell: Shell) -> None:
+def _prompt_setup(setup: Callable, name: str):
     """Prompt the user to run a setup procedure with a `Display` object. It
     will run the procedure if the user enters "y" or "yes". The procedure
     should take a `Display` object as its only argument.
 
     Args:
-        procedure (function): The setup procedure to run.
-        display (Display): The display for printing output.
+        setup (function): The setup function to run.
+        name (str): The name of the setup function.
     """
-    if silent:
-        procedure(display, shell, silent=True)
-        return
+    global silent, _display, _shell
 
+    # run setup function if running in silent mode
+    if silent:
+        setup(_display, _shell, silent=True)
+        return
+    # prompt user for confirmation otherwise
     answer = input(f"Do you want to setup {name}? (y/n [n]) ")
     if answer and answer.lower()[0] == "y":
-        procedure(display, shell)
+        setup(_display, _shell)
 
 
 if __name__ == "__main__":
