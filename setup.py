d8e802225f5d4605deeb4553e8269add49702074
Mohammed Fareed <mohdf.fareed@icloud.com>
2023-04-15 16:58:54 -0400

    Fix setup to import packages in correct order

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-from setup_modules import git, homebrew, macos, python, ssh, zsh
+from setup_modules import ssh
 from utils import shell
 from utils.display import Display
 
 
-def main(display: Display, ssh_dir: str) -> None:
+def setup(display: Display, ssh_dir: str) -> None:
     """Run the main function of the setup script. This function is called when
     the script is run from the command line. By default, the setup is run in
     verbose and debug mode without logging output to a file.
@@ -28,28 +28,14 @@ def main(display: Display, ssh_dir: str) -> None:
 
     # run setup modules
     display.debug("Running setup modules...")
-    homebrew.setup(display)
-    zsh.setup(display)
-    git.setup(display)
-    python.setup(display)
-    macos.setup(display)
-
-    display.success("")
-    display.success("Machine setup complete!")
+    _invoke_setup(display)
+    display.success(""); display.success("Machine setup complete!")
     display.info("Please restart your machine for some changes to apply.")
 
 
-def _init(verbose, debug, no_logging) -> Display:
-    """Initialize the display for printing messages and logging them to a file.
+def main() -> None:
+    """Run the main setup function and handle exceptions.
     """
-    display = Display(verbose, debug, no_logging)
-    display.debug("Debug mode is enabled.") if debug else None
-    display.verbose("Verbose mode is enabled.") if verbose else None
-    display.info("Logging is disabled.") if no_logging else None
-    return display
-
-
-if __name__ == "__main__":
     import argparse
 
     # parse command line arguments
@@ -69,8 +55,37 @@ if __name__ == "__main__":
     display = _init(verbose, debug, no_logging)
 
     try:  # run main function and handle exceptions
-        main(display, args.ssh_dir)
+        setup(display, args.ssh_dir)
     except Exception as exception:
         display.error(exception.__str__())
         display.error("")
         display.error(f"Failed to setup machine.")
+
+
+def _init(verbose, debug, no_logging) -> Display:
+    """Initialize the display for printing messages and logging them to a file.
+    """
+    display = Display(verbose, debug, no_logging)
+    display.debug("Debug mode is enabled.") if debug else None
+    display.verbose("Verbose mode is enabled.") if verbose else None
+    display.info("Logging is disabled.") if no_logging else None
+    return display
+
+
+def _invoke_setup(display: Display) -> None:
+    """Invoke the setup modules.
+
+    Args:
+        display (Display): The display for printing messages.
+    """
+    from setup_modules import git, homebrew, macos, python, zsh
+
+    homebrew.setup(display)
+    zsh.setup(display)
+    git.setup(display)
+    python.setup(display)
+    macos.setup(display)
+
+
+if __name__ == "__main__":
+    main()
