90647543f731a0c39b2122f22c711cf02f45e220
Mohammed Fareed <mohdf.fareed@icloud.com>
2023-07-23 01:47:38 -0400

    Add raspberry pi services

--- core/raspberrypi/setup.sh
+++ core/raspberrypi/setup.sh
@@ -1,14 +1,17 @@
-# REQUIREMENTS: $MACHINE is set to the path of the config files to symlink
+# REQUIREMENTS: $MACHINE set to the path of the machine resource directory
 
 # remove login message
 touch "$HOME/.hushlogin"
-
 # install packages
 . $MACHINE/install.sh
 
 # symlink config files
 ln -sf $MACHINE/micro_settings.json $HOME/.config/micro/settings.json
 ln -sf $MACHINE/zshrc               $HOME/.zshrc
+# load services
+for service in $MACHINE/*.service; do
+    sudo ln -sf $service /etc/systemd/system/$(basename $service)
+done
 
 # set zsh as the default shell
 sudo chsh -s $(which zsh)
