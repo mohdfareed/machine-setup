ec14332045cb6e71ff757607ae49336ee9b6ede9
Mohammed Fareed <mohdf.fareed@icloud.com>
2023-07-22 20:45:41 -0400

    Add raspberry pi items

---
+++ core/raspberrypi/packages.sh
@@ -0,0 +1,11 @@
+# update and upgrade
+sudo apt update && sudo apt full-upgrade -y
+
+# install packages
+sudo apt install git -y
+sudo apt install zsh -y
+sudo apt install bat -y
+sudo apt install exa -y
+sudo apt install micro -y
+sudo apt install zsh-syntax-highlighting -y
+sudo apt install zsh-autosuggestions -y
--- core/raspberrypi/setup.sh
+++ core/raspberrypi/setup.sh
@@ -1,14 +1,3 @@
-# update and upgrade
-sudo apt update && sudo apt full-upgrade -y
-# install packages
-sudo apt install git -y
-sudo apt install zsh -y
-sudo apt install bat -y
-sudo apt install exa -y
-sudo apt install zsh-syntax-highlighting -y
-sudo apt install zsh-autosuggestions -y
-sudo apt install spaceship -y
-
 # python version manager
 curl https://pyenv.run | bash
 # python dependencies
@@ -16,5 +5,13 @@ sudo apt install build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev curl \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
 
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# pure prompt
+mkdir -p "$HOME/.zsh"
+git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+
+# remove login message
+touch "$HOME/.hushlogin"
 # set zsh as the default shell
-chsh -s /bin/zsh
+sudo chsh -s $(which zsh)
