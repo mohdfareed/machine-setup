2f14f70d7b1faeee45e76a438f4c848697659f6f
Mohammed Fareed <mohdf.fareed@icloud.com>
2023-08-04 20:08:24 -0400

    Refactor `shell` setup script

--- core/shell.py
+++ core/shell.py
@@ -2,43 +2,58 @@
 machine.
 """
 
-from config import zshrc
-from utils import abspath, create_file, shell, symlink
-from utils.logger import Display
+import config
+import utils
 
-DISPLAY: Display = Display(no_logging=True)
-"""The default display for printing messages."""
-
-_zshrc: str = abspath("~/.zshrc")
+ZPROFILE: str = utils.abspath("~/.zprofile", resolve_links=False)
+"""The path to the zsh profile file symlink."""
+ZSHRC: str = utils.abspath("~/.zshrc", resolve_links=False)
 """The path to the zsh configuration file symlink."""
+ZSHENV: str = utils.abspath("~/.zshenv", resolve_links=False)
+"""The path to the zsh environment file symlink."""
+
+printer = utils.Printer("shell")
+"""The ZSH setup printer."""
+shell = utils.Shell(printer.debug, printer.error)
+"""The ZSH shell instance."""
 
 
-def setup(display=DISPLAY) -> None:
-    """Setup the shell on a new machine by symlinking its configuration files.
-
-    A `Display` object is used to print messages and log them to a file. A
-    non-logging `Display` object is used by default.
-
-    Args:
-        display (Display, optional): The display for printing messages.
-    """
-    display.header("Setting up shell...")
+def setup() -> None:
+    """Setup the shell environment on a machine."""
+    printer.info("Setting up shell...")
 
     # check if homebrew is installed
-    if shell.run("command -v brew", display.debug, display.error) != 0:
-        raise RuntimeError("Could not find Homebrew.")
-    display.verbose("Homebrew was found.")
+    if shell(["command", "-v", "brew"], silent=True)[1] != 0:
+        raise RuntimeError("Could not find Homebrew")
 
-    # symlink configuration file
-    symlink(zshrc, _zshrc)
-    display.verbose(f"Symlinked: {zshrc}")
-    display.verbose(f"       to: {_zshrc}")
-    # remove last login time prompt
-    create_file("~/.hushlogin")
-    display.verbose("Removed last login time prompt.")
+    # install omz and symlink config files
+    install_omz()
+    utils.symlink(config.zshrc, ZSHRC)
+    utils.symlink(config.zshenv, ZSHENV)
+    utils.symlink(config.zprofile, ZPROFILE)
+    printer.success("Shell setup complete")
 
-    display.success("Shell was setup successfully.")
+
+def install_omz():
+    printer.info("Installing oh-my-zsh...")
+
+    # load installation environment
+    cmd = f"source {config.zsh_env} > /dev/null && echo $ZSH"
+    env = dict(ZSH=shell(cmd, silent=True)[0])
+
+    # install oh-my-zsh
+    shell(["rm", "-rf", env["ZSH"]], silent=True)
+    cmd = '"$(curl -fsSL https://git.io/JvzfK)" "" --unattended'
+    if shell(f"sh -c {cmd}", silent=True, env=env)[1] != 0:
+        raise RuntimeError("Failed to install oh-my-zsh")
+    printer.debug("Installed oh-my-zsh")
 
 
 if __name__ == "__main__":
-    setup()
+    import argparse
+
+    import core
+
+    parser = argparse.ArgumentParser(description="Shell setup script.")
+    args = parser.parse_args()
+    core.run(setup, printer, "Failed to setup shell")
