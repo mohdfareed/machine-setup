fc23f789197f7a9d5b6ea8860245444376649b58
Mohammed Fareed <mohdf.fareed@icloud.com>
2023-08-17 18:19:59 -0400

    Configure `vim`

--- config/vim/lua/plugins/core.lua
+++ config/vim/lua/plugins/core.lua
@@ -1,25 +1,25 @@
 return {
-	-- dracula theme
-	{
-		"Mofiqul/dracula.nvim",
-		opts = {
-			transparent_bg = true,
-		},
-	},
-
-	-- set theme colorscheme
-	{
+	{ -- configure LazyVim
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "dracula",
+			colorscheme = "onedark",
 		},
 	},
 
-	-- symbols-outline
-	{
+	{ -- symbols-outline
 		"simrat39/symbols-outline.nvim",
 		cmd = "SymbolsOutline",
 		keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
 		config = true,
 	},
+
+	{ -- fix notifications background color
+		"rcarriga/nvim-notify",
+		opts = {
+			background_color = "#000000",
+		},
+	},
+
+	-- disabled plugins
+	{ "akinsho/bufferline.nvim", enabled = false },
 }
---
+++ config/vim/lua/plugins/onedark.lua
@@ -0,0 +1,12 @@
+return {
+	{ -- one dark theme
+		"navarasu/onedark.nvim",
+		opts = {
+			style = "darker",
+			transparent = true,
+			lualine = {
+				transparent = true,
+			},
+		},
+	},
+}
