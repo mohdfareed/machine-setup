0a0827b6819dcc9a4c8df0e7fbbf696204a95e84
Mohammed Fareed <mohdf.fareed@icloud.com>
2023-08-17 22:04:22 -0400

    Use spaces for indents

--- config/vim/lua/config/options.lua
+++ config/vim/lua/config/options.lua
@@ -4,3 +4,8 @@
 local opt = vim.opt
 opt.wrap = true -- enable line wrap
 opt.colorcolumn = "80" -- line length marker at 80 columns
+
+-- default to 4 spaces for tab
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.expandtab = true
--- config/zsh/config.sh
+++ config/zsh/config.sh
@@ -8,18 +8,18 @@ ZSH_THEME=""                   # use external theme
 zstyle ':omz:update' mode auto # update automatically without asking
 
 plugins+=(
-    git               # git aliases
-    colored-man-pages # colorize man pages
+	git               # git aliases
+	colored-man-pages # colorize man pages
+	tmux              # tmux integration
 )
 # load oh-my-zsh
 source $ZSH/oh-my-zsh.sh
 
-
 # Shell Configuration
 # =============================================================================
 
-unsetopt auto_list             # don't echo choices on ambiguous completion
-setopt menu_complete           # auto-complete with menu selection
+unsetopt auto_list   # don't echo choices on ambiguous completion
+setopt menu_complete # auto-complete with menu selection
 
 # auto-completion
 zstyle ':completion:*' completer _expand _complete _correct _approximate
@@ -27,25 +27,25 @@ zstyle ':completion:*' format '%BCompleting %d:%b'
 zstyle ':completion:*' group-name ''
 
 # completions colors
-zstyle ':completion:*:options'     list-colors '=(#b)(*)-- *==34' '=^(-- *)=34'
-zstyle ':completion:*:parameters'  list-colors '=*=34'
-zstyle ':completion:*:commands'    list-colors '=*=1;32'
-zstyle ':completion:*:builtins'    list-colors '=*=1;32'
-zstyle ':completion:*:functions'   list-colors '=*=1;35'
-zstyle ':completion:*:aliases'     list-colors '=*=1;35'
+zstyle ':completion:*:options' list-colors '=(#b)(*)-- *==34' '=^(-- *)=34'
+zstyle ':completion:*:parameters' list-colors '=*=34'
+zstyle ':completion:*:commands' list-colors '=*=1;32'
+zstyle ':completion:*:builtins' list-colors '=*=1;32'
+zstyle ':completion:*:functions' list-colors '=*=1;35'
+zstyle ':completion:*:aliases' list-colors '=*=1;35'
 zstyle ':completion:*:systemctl:*' list-colors '=(#b)*(-- *)==34' '=-- *=34'
 
 # completion format of processes
 zstyle ':completion:*:*:*:*:processes' command \
-"ps -u $USERNAME -o pid,%cpu,%mem,cputime,command"
+	"ps -u $USERNAME -o pid,%cpu,%mem,cputime,command"
 zstyle ':completion:*:*:kill:*:processes' list-colors \
-'=(#b) #([0-9]#) #*[0-9]#:[0-9]#[:.][0-9]# #(*)=34=1;31=32'
+	'=(#b) #([0-9]#) #*[0-9]#:[0-9]#[:.][0-9]# #(*)=34=1;31=32'
 
 # pure prompt
 PURE_PROMPT_SYMBOL='➜'
-autoload -U promptinit; promptinit
+autoload -U promptinit
+promptinit
 prompt pure
-
 
 # Functions and Aliases
 # =============================================================================
@@ -59,8 +59,7 @@ alias llst='lls -T'
 
 # time the startup of shell
 ztime() {
-    usage="usage: $0 [iterations]"
-    if (($# > 1)); then echo $usage && return 1; fi
-    for i in $(seq 1 ${1-1}); do time $SHELL -i -c exit; done
+	usage="usage: $0 [iterations]"
+	if (($# > 1)); then echo $usage && return 1; fi
+	for i in $(seq 1 ${1-1}); do time $SHELL -i -c exit; done
 }
-
