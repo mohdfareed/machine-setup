8fccf1bf474115eff4a7b11efaf2e68e8113f740
Mohammed Fareed <mohdf.fareed@icloud.com>
2023-08-20 02:56:16 -0400

    Fix raspberry pi packages installation

--- raspberrypi/scripts/install.sh
+++ raspberrypi/scripts/install.sh
@@ -7,7 +7,7 @@ sudo apt update
 # machine packages
 echo "Installing machine packages..."
 sudo apt install -y zsh zsh-syntax-highlighting zsh-autosuggestions git exa bat
-sudo apt install -y tmux neovim lazygit ripgrep fd python3-venv npm
+sudo apt install -y tmux neovim ripgrep fd-find python3-venv npm
 sudo apt upgrade -y
 
 # snap store packages
@@ -16,7 +16,11 @@ sudo apt install -y snapd
 sudo snap install core
 sudo snap install btop
 sudo snap install nvim --classic
+sudo snap install go --classic
 sudo snap refresh
+
+# go packages
+go install github.com/jesseduffield/lazygit@latest
 
 # clean packages
 echo "Cleaning packages..."
