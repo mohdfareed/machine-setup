0152c05293f6c467975dd1af33aecb7425b06518
Mohammed Fareed <mohdf.fareed@icloud.com>
2024-04-27 21:08:03 -0400

    Use touch ID for sudo authentication

--- scripts/macos.py
+++ scripts/macos.py
@@ -8,6 +8,14 @@ import utils
 
 VSCODE = "~/Library/Application Support/Code/User"
 """The path to the VSCode user settings directory on macOS."""
+PAM_SUDO = "/etc/pam.d/sudo_local"
+"""The path to the sudo PAM configuration file on macOS."""
+PAM_SUDO_MODULE = "pam_tid.so"
+"""The name of the PAM module to enable Touch ID for sudo."""
+PAM_SUDO_CONTENT = f"""
+auth       sufficient     {PAM_SUDO_MODULE}
+"""
+"""The content to add to the sudo PAM configuration file to enable Touch ID."""
 
 LOGGER = logging.getLogger(__name__)
 """The macOS setup logger."""
@@ -28,6 +36,17 @@ def setup() -> None:
     LOGGER.info("Setting system preferences...")
     utils.run_cmd(f". {config.macos_preferences}")
     LOGGER.debug("System preferences set.")
+
+    # use touch ID for sudo
+    LOGGER.info("Setting up Touch ID for sudo...")
+    with open(PAM_SUDO, "r", encoding="utf-8") as f:
+        lines = f.read()
+    if PAM_SUDO_MODULE not in lines:
+        with open(PAM_SUDO, "a", encoding="utf-8") as f:
+            f.write(PAM_SUDO_CONTENT)
+        LOGGER.debug("Touch ID for sudo set up.")
+    else:
+        LOGGER.debug("Touch ID for sudo already set up.")
 
     LOGGER.info("macOS setup complete.")
 
