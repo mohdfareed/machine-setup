f335db0baed55e7ca28b655fffcf790ce10ada71
Mohammed Fareed <mohdf.fareed@icloud.com>
2024-09-14 05:09:43 -0400

    Fix wsl setup

--- machines/windows/setup.py
+++ machines/windows/setup.py
@@ -20,30 +20,30 @@ def setup(private_machine: str | None = None) -> None:
     winget = WinGet()
     scoop = Scoop()
 
-    # setup shell
-    shell.setup_windows(windows.ps_profile)
-    shell.install_powershell(winget)
-    shell.install_nvim(winget)
-    shell.install_btop(scoop)
+    # # setup shell
+    # shell.setup_windows(windows.ps_profile)
+    # shell.install_powershell(winget)
+    # shell.install_nvim(winget)
+    # shell.install_btop(scoop)
 
-    # setup ssh
-    ssh.generate_key_pair("personal")
-    ssh.setup(windows.ssh_config)
-    ssh.setup_server(None)
+    # # setup ssh
+    # ssh.generate_key_pair("personal")
+    # ssh.setup(windows.ssh_config)
+    # ssh.setup_server(None)
 
-    # setup core machine
-    git.setup(winget)
-    vscode.setup(winget)
-    vscode.setup_tunnels("pc")
-    tailscale.setup(None)
-    scoop.setup_fonts()
+    # # setup core machine
+    # git.setup(winget)
+    # vscode.setup(winget)
+    # vscode.setup_tunnels("pc")
+    # tailscale.setup(None)
+    # scoop.setup_fonts()
 
-    # setup dev tools
-    scripts.setup_docker(winget)
-    scripts.setup_python(scoop)
-    scripts.setup_node(winget)
-    winget.install("GoLang.Go")
-    winget.install("Microsoft.DotNet.SDK")
+    # # setup dev tools
+    # scripts.setup_docker(winget)
+    # scripts.setup_python(scoop)
+    # scripts.setup_node(winget)
+    # winget.install("GoLang.Go")
+    # winget.install("Microsoft.DotNet.SDK")
     setup_wsl()  # install ubuntu wsl
 
     # extras
@@ -59,11 +59,9 @@ def setup_wsl(wsl_module="machines.windows.wsl") -> None:
     LOGGER.info("Setting up WSL...")
     utils.shell.run("wsl --install", info=True)
 
-    wsl_commands = (
-        f"cd {config.MACHINE} && ",
-        f"python -m {wsl_module}",
-    )
-    utils.shell.run(f'wsl -e bash -c "{wsl_commands}"', info=True)
+    wsl_command = f"cd {config.MACHINE} && python -m {wsl_module}"
+    LOGGER.warning("Run the following command in WSL: %s", wsl_command)
+    utils.shell.run(f"wsl -e bash -c {wsl_command}")
     LOGGER.info("WSL setup complete.")
 
 
