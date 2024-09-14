3650901e6249e584555bf3b37997d2c48b072fb7
Mohammed Fareed <mohdf.fareed@icloud.com>
2024-09-14 16:11:21 -0400

    Fix importing issues

--- machines/windows/__init__.py
+++ machines/windows/__init__.py
@@ -1,8 +1,10 @@
 """Windows specific setup module."""
 
 import os as _os
+from typing import Callable as _Callable
 
-from .setup import setup_wsl
+import utils as _utils
+from machines import LOGGER
 
 config = _os.path.join(_os.path.dirname(_os.path.realpath(__file__)), "config")
 """The path of macOS configuration files."""
@@ -12,3 +14,16 @@ ps_profile = _os.path.join(config, "ps_profile.ps1")
 
 ssh_config = _os.path.join(config, "ssh.config")
 """The path of the global ssh config file."""
+
+
+# FIXME: find better way to run wsl setup script, doesn't work as is
+def setup_wsl(wsl_setup_handler: _Callable) -> None:
+    """Setup WSL on a new machine."""
+    LOGGER.info("Setting up WSL...")
+    _utils.shell.run("wsl --install", info=True)
+    current_shell = _utils.shell.EXECUTABLE
+
+    _utils.shell.EXECUTABLE = _utils.shell.SupportedExecutables.WSL
+    wsl_setup_handler()
+    _utils.shell.EXECUTABLE = current_shell
+    LOGGER.info("WSL setup complete.")
--- machines/windows/setup.py
+++ machines/windows/setup.py
@@ -1,7 +1,5 @@
 """Setup module containing a `setup` function for setting up Windows."""
 
-from typing import Callable as _Callable
-
 import config
 import scripts
 import utils
@@ -9,7 +7,7 @@ from machines import LOGGER, windows
 from scripts import git, shell, ssh, tailscale, vscode
 from scripts.package_managers import Scoop, WinGet
 
-from . import wsl
+# from . import setup_wsl, wsl
 
 
 def setup(private_machine: str | None = None) -> None:
@@ -21,34 +19,34 @@ def setup(private_machine: str | None = None) -> None:
         config.link_private_config(private_machine)
 
     # setup package managers
-    # winget = WinGet()
+    winget = WinGet()
     scoop = Scoop()
 
-    # # setup shell
-    # shell.setup_windows(windows.ps_profile)
-    # shell.install_powershell(winget)
-    # shell.install_nvim(winget)
-    # shell.install_btop(scoop)
+    # setup shell
+    shell.setup_windows(windows.ps_profile)
+    shell.install_powershell(winget)
+    shell.install_nvim(winget)
+    shell.install_btop(scoop)
 
-    # # setup ssh
-    # ssh.generate_key_pair("personal")
-    # ssh.setup(windows.ssh_config)
-    # ssh.setup_server(None)
+    # setup ssh
+    ssh.generate_key_pair("personal")
+    ssh.setup(windows.ssh_config)
+    ssh.setup_server(None)
 
-    # # setup core machine
-    # git.setup(winget)
-    # vscode.setup(winget)
-    # vscode.setup_tunnels("pc")
-    # tailscale.setup(None)
-    # scoop.setup_fonts()
+    # setup core machine
+    git.setup(winget)
+    vscode.setup(winget)
+    vscode.setup_tunnels("pc")
+    tailscale.setup(None)
+    scoop.setup_fonts()
 
-    # # setup dev tools
-    # scripts.setup_docker(winget)
-    # scripts.setup_python(scoop)
-    # scripts.setup_node(winget)
-    # winget.install("GoLang.Go")
-    # winget.install("Microsoft.DotNet.SDK")
-    setup_wsl(wsl.setup)  # install ubuntu wsl
+    # setup dev tools
+    scripts.setup_docker(winget)
+    scripts.setup_python(scoop)
+    scripts.setup_node(winget)
+    winget.install("GoLang.Go")
+    winget.install("Microsoft.DotNet.SDK")
+    # setup_wsl(wsl.setup)  # install ubuntu wsl
 
     # extras
     scoop.add_bucket("extras")
@@ -56,18 +54,6 @@ def setup(private_machine: str | None = None) -> None:
 
     LOGGER.info("Windows setup complete.")
     LOGGER.warning("Restart for some changes to apply.")
-
-
-def setup_wsl(wsl_setup_handler: _Callable) -> None:
-    """Setup WSL on a new machine."""
-    LOGGER.info("Setting up WSL...")
-    utils.shell.run("wsl --install", info=True)
-    current_shell = utils.shell.EXECUTABLE
-
-    utils.shell.EXECUTABLE = utils.shell.SupportedExecutables.WSL
-    wsl_setup_handler()
-    utils.shell.EXECUTABLE = current_shell
-    LOGGER.info("WSL setup complete.")
 
 
 if __name__ == "__main__":
